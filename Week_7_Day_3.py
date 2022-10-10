b23a848e46addd0f9b03bdffbcaea333981fd67e
Charan Karthik <secharank@gmail.com>
2022-10-09 23:24:32 -0700

    setup from previous algo

--- Week_7_Day_3.py
+++ Week_7_Day_3.py
@@ -7,3 +7,22 @@ class SLL:
     def __init__(self):
         self.head = None
     
+    def printSLL(self):
+        currentNode = self.head
+        if currentNode == None:
+            print("Empty SLL")
+        while currentNode is not None:
+            print(currentNode.value)
+            currentNode = currentNode.next
+    
+    def isEmpty(self):
+        # return self.head == None
+        return True if self.head == None else False
+        # ^ practicing ternary in python
+    
+    def addToFront(self, node):
+        if(self.head == None):
+            self.head = node
+        else:
+            node.next = self.head
+            self.head = node
