c53a5fbf29c1d25760a3f02a3fa1007e17b5d426
Charan Karthik <secharank@gmail.com>
2022-10-09 22:40:24 -0700

    addToFront() method working

--- Week_7_Day_2.py
+++ Week_7_Day_2.py
@@ -6,8 +6,30 @@ class Node:
 class SLL:
     def __init__(self):
         self.head = None
+
+    def printSLL(self):
+        currentNode = self.head
+        if currentNode == None:
+            print("Empty SLL")
+        while currentNode is not None:
+            print(currentNode.value)
+            currentNode = currentNode.next
     
-    def isEmpty():
+    def isEmpty(self):
         return self.head == None
         # return True if self.head == None else False
         # ^ practicing ternary in python
+    
+    def addToFront(self, node):
+        if(self.head == None):
+            self.head = node
+        else:
+            node.next = self.head
+            self.head = node
+
+
+mySLL = SLL()
+mySLL.addToFront(Node(10))
+mySLL.addToFront(Node(15))
+mySLL.addToFront(Node(20))
+mySLL.printSLL()
