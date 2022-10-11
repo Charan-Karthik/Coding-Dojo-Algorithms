0e22a0aca535bc6e4ec0a7e78ab58485e4979f33
Charan Karthik <secharank@gmail.com>
2022-10-11 16:52:49 -0700

    twoSum (O(n)) solution complete

--- Week_12_Day_4.py
+++ Week_12_Day_4.py
@@ -10,6 +10,22 @@ def twoSumSlow(nums, target):
             else:
                 j += 1
 
-print(twoSumSlow([2, 11, 7, 15], 9)) # [0,2]
-print(twoSumSlow([3, 2, 4], 6)) # [1,2]
-print(twoSumSlow([3, 3], 6)) # [0,1]
+# print(twoSumSlow([2, 11, 7, 15], 9)) # [0,2]
+# print(twoSumSlow([3, 2, 4], 6)) # [1,2]
+# print(twoSumSlow([3, 3], 6)) # [0,1]
+
+
+def twoSumFast(nums, target):
+    hashmap = {}
+
+    for i in range(len(nums)):
+        if nums[i] not in hashmap:
+            hashmap[nums[i]] = i
+        
+        difference = target - nums[i]
+        if difference in hashmap and hashmap[difference] != i:
+            return [hashmap[difference], i]
+
+print(twoSumFast([2, 11, 7, 15], 9)) # [0,2]
+print(twoSumFast([3, 2, 4], 6)) # [1,2]
+print(twoSumFast([3, 3], 6)) # [0,1]
