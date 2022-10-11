e96d7c557520cd5e1a0f97784799e48ce213d64c
Charan Karthik <secharank@gmail.com>
2022-10-11 16:47:45 -0700

    twoSum O(n^2) solution

--- Week_12_Day_4.py
+++ Week_12_Day_4.py
@@ -1,1 +1,15 @@
 # TwoSum Python
+
+def twoSumSlow(nums, target):
+    for i in range(len(nums)):
+        j = i + 1 
+
+        while j < len(nums):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+            else:
+                j += 1
+
+print(twoSumSlow([2, 11, 7, 15], 9)) # [0,2]
+print(twoSumSlow([3, 2, 4], 6)) # [1,2]
+print(twoSumSlow([3, 3], 6)) # [0,1]
