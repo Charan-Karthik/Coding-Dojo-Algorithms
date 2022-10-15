4888d05dfdd41783e6c9baf10880c09508a53396
Charan Karthik <secharank@gmail.com>
2022-10-14 22:56:33 -0700

    solution posted

--- Week_14_Day_2.py
+++ Week_14_Day_2.py
@@ -1,7 +1,36 @@
 # Rehash an incorrectly hashed string by combining letter count occurrences and alphabetizing them.
 
 def rehash(string):
-    return
+    hashmap = {}
+    numStr = ""
+    lastLetter = None
+
+    for char in string:
+        if char.isalpha():
+            if lastLetter in hashmap:
+                hashmap[lastLetter] += int(numStr)
+                numStr = ""
+            if char in hashmap:
+                lastLetter = char
+                continue
+            else:
+                lastLetter = char
+                hashmap[char] = 0
+        else:
+            numStr += char
+    hashmap[lastLetter] += int(numStr)
+
+    letters = []
+    for key in hashmap:
+        letters.append(key)
+    letters.sort()
+
+    resultStr = ""
+    for letter in letters:
+        resultStr += letter
+        resultStr += str(hashmap[letter])
+
+    return resultStr
 
 string1 = "b70a164c32a20c10"
 expected1 = "a184b70c42"
