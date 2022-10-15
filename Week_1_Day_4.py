74f964894f7cbfc79c86ab12817ca3645c85f5e4
Charan Karthik <secharank@gmail.com>
2022-10-15 00:22:29 -0700

    solution complete

--- Week_1_Day_4.py
+++ Week_1_Day_4.py
@@ -1,1 +1,16 @@
 # reverse an array in place
+
+def reverse(arr):
+    if len(arr) < 1:
+        return []
+    if len(arr) == 1:
+        return arr
+
+    for i in range(int(len(arr)/2)):
+        temp = arr[i]
+        arr[i] = arr[len(arr)-1-i]
+        arr[len(arr)-1-i] = temp
+    
+    return arr
+
+print(reverse(["a", "b", "c", "d", "e"]))
