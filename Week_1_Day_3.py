8fc06b70440ffbb23c8ede7b67d9cadb18c12a10
Charan Karthik <secharank@gmail.com>
2022-10-15 00:16:25 -0700

    solution complete

--- Week_1_Day_3.py
+++ Week_1_Day_3.py
@@ -1,4 +1,20 @@
 # Check if an array is a palindrome
 
 def isPalindrome(arr):
-    return
+    left = 0
+    right = len(arr) -1
+
+    while left < len(arr)/2:
+        if arr[left] != arr[right]:
+            return False
+        else:
+            left += 1
+            right -= 1
+
+    return True
+
+print(isPalindrome([0, 1, 1, 0]))
+print(isPalindrome([0, 1, 2, 1, 0]))
+print(isPalindrome([1]))
+print(isPalindrome([]))
+print(isPalindrome([0, 1, 2, 3, 1, 0]))
