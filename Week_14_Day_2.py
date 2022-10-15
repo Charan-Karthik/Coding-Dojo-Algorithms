b3b86cdece7d309e6ca819bcf4dd88d090e36f97
Charan Karthik <secharank@gmail.com>
2022-10-14 22:56:15 -0700

    new algo initial commit

---
+++ Week_14_Day_2.py
@@ -0,0 +1,9 @@
+# Rehash an incorrectly hashed string by combining letter count occurrences and alphabetizing them.
+
+def rehash(string):
+    return
+
+string1 = "b70a164c32a20c10"
+expected1 = "a184b70c42"
+
+print(rehash(string1) == expected1)
