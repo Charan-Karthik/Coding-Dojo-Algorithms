0a02130b4935c317bc7bb4d4a64a2ed8d0a64f5b
Charan Karthik <secharank@gmail.com>
2022-10-16 12:09:55 -0700

    solution complete

--- Week_2_Day_5.py
+++ Week_2_Day_5.py
@@ -1,1 +1,24 @@
 # Flatten a 2D Array
+
+arr = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+];
+
+other = [
+    [1,2,3,4,5,6],
+    [2,7,4,1,5,5]
+]
+
+def flatten(arr2d):
+    flattened = []
+
+    for i in range(len(arr2d)):
+        for j in range(len(arr2d[i])):
+            flattened.append(arr2d[i][j])
+    
+    return flattened
+
+print(flatten((arr)))
+print(flatten((other)))
