fc3a51fde5e4adfe4781773879d494489e1a49ff
Charan Karthik <secharank@gmail.com>
2022-10-16 12:38:59 -0700

    solution with no built-ins complete

--- Week_2_Day_5.py
+++ Week_2_Day_5.py
@@ -34,5 +34,32 @@ def flip(arr2d):
     
     return arr2d
 
-print(flip(arr))
-print(flip(other))
+# print(flip(arr))
+# print(flip(other))
+
+def flip_noBuiltIns(arr2d):
+    start, end = 0, len(arr2d)-1
+
+    while start < int(len(arr2d)/2):
+        temp = arr2d[start]
+        arr2d[start] = arr2d[end]
+        arr2d[end] = temp
+        start += 1
+        end -= 1
+    
+    for i in range(len(arr2d)):
+        # print(len(arr2d[i]))
+        left, right = 0, len(arr2d[i])-1
+
+        while left < int((len(arr2d[i])/2)):
+            temp = arr2d[i][left]
+            arr2d[i][left] = arr2d[i][right]
+            arr2d[i][right] = temp
+            left += 1
+            right -= 1
+    
+    return arr2d
+
+
+# print(flip_noBuiltIns(arr))
+# print(flip_noBuiltIns(other))
