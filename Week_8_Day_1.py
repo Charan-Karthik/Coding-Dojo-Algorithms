454649b056ee04cd32a631161d73b609936bb894
Charan Karthik <secharank@gmail.com>
2022-10-20 23:09:06 -0700

    new algo initial commit (BST)

---
+++ Week_8_Day_1.py
@@ -0,0 +1,11 @@
+# Binary Tree Introduction
+
+class BSTNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BST:
+    def __init__(self):
+        self.root = None
