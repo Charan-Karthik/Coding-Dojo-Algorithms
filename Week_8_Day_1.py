7570e2f8ff5904924301048841ae0e6e1f267b76
Charan Karthik <secharank@gmail.com>
2022-10-20 23:13:08 -0700

    isEmpty and insert (initial code)

--- Week_8_Day_1.py
+++ Week_8_Day_1.py
@@ -9,3 +9,25 @@ class BSTNode:
 class BST:
     def __init__(self):
         self.root = None
+    
+    def isEmpty(self):
+        return self.root == None
+    
+    def insert(self, node, current = this.root):
+        
+        if current == None:
+            this.root = node
+            return
+        
+        if node.value > current.value:
+            if current.right == None:
+                current.right = node
+                return
+            else:
+                self.insert(node, current.right)
+        elif node.value < current.value:
+            if current.left == None:
+                current.left = node
+                return
+            else:
+                self.insert(node, current.left)
