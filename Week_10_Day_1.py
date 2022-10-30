b122e2fb1f307ceb94264bd02871b1a2ebdc865e
Charan Karthik <secharank@gmail.com>
2022-10-29 19:27:18 -0700

    methods completed

--- Week_10_Day_1.py
+++ Week_10_Day_1.py
@@ -11,16 +11,48 @@ class Stack:
         self.top = None
     
     def push(self, newNode):
-        return
+        newNode.next = self.top
+        self.top = newNode
+        return self
     
     def pop(self, newNode):
-        return
+        
+        if self.top == None:
+            return None
+
+        removedNode = self.top
+        self.top = removedNode.next
+
+        if removedNode.next != None:
+            removedNode.next = None
+        
+        return removedNode
 
     def peek(self):
-        return
+        return self.top
     
     def isEmpty(self):
-        return
+        return self.top == None
     
     def getLength(self):
-        return
+        if self.top == None:
+            return 0
+
+        if self.top.next == None:
+            return 1
+        
+        tempStack = Stack()
+        while self.top != None:
+            nodeToTransfer = self.pop()
+            tempStack.push(nodeToTransfer)
+        
+        count = 0
+        while tempStack.top != None:
+            tempNode = tempStack.pop()
+            self.push(tempNode)
+            count += 1
+
+        return count
+
+
+# Needs to be tested
