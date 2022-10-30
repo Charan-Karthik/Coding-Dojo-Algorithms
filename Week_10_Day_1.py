7593d5ea98781f19b02e7a34e9f90a977841889c
Charan Karthik <secharank@gmail.com>
2022-10-29 19:23:21 -0700

    classes and methods added

--- Week_10_Day_1.py
+++ Week_10_Day_1.py
@@ -1,2 +1,26 @@
 # Stack Data Structure
 # Initial Attempt with Python
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class Stack:
+    def __init__(self):
+        self.top = None
+    
+    def push(self, newNode):
+        return
+    
+    def pop(self, newNode):
+        return
+
+    def peek(self):
+        return
+    
+    def isEmpty(self):
+        return
+    
+    def getLength(self):
+        return
