939a869cfdd6933554012be4b903b3309a60189b
Charan Karthik <secharank@gmail.com>
2022-10-30 22:19:07 -0700

    methods completed

--- Week_10_Day_2.py
+++ Week_10_Day_2.py
@@ -12,10 +12,29 @@ class Queue:
         self.back = None
 
     def enqueue(self, node):
-        return
+        if self.front == None:
+            self.front = node
+            self.back = node
+        else:
+            self.back.next = node
+            self.back = node
+
+        return self
     
-    def dequeue(self, node):
-        return
+    def dequeue(self):
+        if self.front == None:
+            return None
+        
+        if self.front == self.back: # OR if self.front.next == None
+            temp = self.front
+            self.front = None
+            self.back = None
+            return temp
+        else:
+            temp = self.front
+            self.front = temp.next
+            temp.next = None
+            return temp
     
     def peek(self):
         return self.front
@@ -24,4 +43,23 @@ class Queue:
         return self.front == None
     
     def count(self):
-        return
+
+        if self.front == None:
+            return 0
+        
+        if self.front == self.back:
+            return 1
+
+        count = 0
+
+        tempQueue = Queue()
+
+        while self.front != None:
+            tempNode = self.dequeue()
+            tempQueue.enqueue(tempNode)
+            count += 1
+        
+        while tempQueue.front != None:
+            self.enqueue(tempQueue.dequeue())
+
+        return count
