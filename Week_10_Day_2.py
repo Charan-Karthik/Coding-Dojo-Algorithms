0d8d04b315e3d08bbe2d90bb2d367a0254c749fe
Charan Karthik <secharank@gmail.com>
2022-10-30 22:13:03 -0700

    new algo initial commit

---
+++ Week_10_Day_2.py
@@ -0,0 +1,12 @@
+# Queue Data Structure Initial Practice
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class Queue:
+    def __init__(self):
+        self.front = None
+        self.back = None
