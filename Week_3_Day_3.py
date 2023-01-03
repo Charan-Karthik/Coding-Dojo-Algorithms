1d3bda6faf14fe23466a47b5a6c2ffaf8334a44f
Charan Karthik <secharank@gmail.com>
2023-01-03 15:53:20 -0800

    decoding complete

--- Week_3_Day_3.py
+++ Week_3_Day_3.py
@@ -1,5 +1,3 @@
-# NEED TO COMPLETE
-
 # You are given a string that may contain sequences of consecutive characters.
 # Create a function to shorten a string by including the character, then the 
 # number of times it appears. For "aaaabbcddd" return "a4b2c1d3"
@@ -21,7 +19,36 @@ def encode(string):
 ## make sure to handle special cases such as "g14f12" and beyond
 
 def decode(string):
-    pass
+
+    res = []
+
+    i = 0
+    while i < len(string):
+        if string[i].isalpha():
+            letter = string[i]
+            i += 1
+        else:
+            j = i
+            while j < len(string) and string[j].isdigit():
+                j += 1
+            
+            # print(i, j-1)
+            if i != j-1:
+                number = int(string[i]+string[j-1])
+            else:
+                number = int(string[i])
+            
+            while number > 0:
+                res.append(letter)
+                number -= 1
+            
+            i = j
+        
+    return ''.join(res)
 
 
 print(encode('aaaabbcddd'))
+print(encode('ggggggggggggggffffffffffff'))
+
+print(decode('g14f12'))
+print(decode('a3b2c1d3'))
