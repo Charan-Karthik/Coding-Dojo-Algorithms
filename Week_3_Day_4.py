07faa3b01af5b6e744c65cc9b7c603603c46379f
Charan Karthik <secharank@gmail.com>
2023-01-03 15:56:31 -0800

    isPalindrome complete

--- Week_3_Day_4.py
+++ Week_3_Day_4.py
@@ -1,9 +1,25 @@
 # NEED TO COMPLETE
 
-# Create a function that returns a boolean whether the string is a strict palindrome. 
+# Create a function that returns a boolean whether the string is a strict palindrome. Assume input string does not contain any spaces.
 # Do not ignore spaces, punctuation and capitalization
+
+def isPalindrome(string):
+    left, right = 0, len(string)-1
+
+    while left < right:
+        if string[left] != string[right]:
+            return False
+        
+        left += 1
+        right -= 1
+    
+    return True
 
 
 # Longest Palindrome
 # For this challenge, we will look not only at the entire string provided, but also at the substrings within it.
 # Return the longest palindromic substring. 
+
+
+print(isPalindrome('racecar'))
+print(isPalindrome('tacodog'))
