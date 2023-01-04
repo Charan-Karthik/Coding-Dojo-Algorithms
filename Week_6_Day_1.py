d3bbcb7e1ee0452cfc530c8e8c8b0eaa0d1aa243
Charan Karthik <secharank@gmail.com>
2023-01-04 14:57:38 -0800

    recursive Fibonacci

--- Week_6_Day_1.py
+++ Week_6_Day_1.py
@@ -8,3 +8,16 @@ def sigma(num):
     return num + sigma(num-1)
 
 print(sigma(4))
+
+
+
+def fib(num):
+    # Base cases since the first two terms in the Fibonacci sequence are always 0, 1.
+    if num == 1:
+        return 0
+    elif num == 2:
+        return 1
+
+    return fib(num-1) + fib(num-2)
+
+print(fib(6))
