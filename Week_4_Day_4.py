00404d96cc2e2879f9d1ba7a21f9907fc1fcd49b
Charan Karthik <secharank@gmail.com>
2023-01-03 16:15:26 -0800

    NEED TO WORK ON 'CHECK IF STRING IS A ROTATION'

--- Week_4_Day_4.py
+++ Week_4_Day_4.py
@@ -4,3 +4,7 @@ def rotateString(string, num):
     charIdxToRotateOn = len(string)-num
 
     return string[charIdxToRotateOn:] + string[0:charIdxToRotateOn]
+
+
+def checkIfStringIsARotation(string1, string2):
+    pass
