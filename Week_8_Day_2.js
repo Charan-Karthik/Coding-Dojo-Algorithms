03f8a9118bc3d6d1f6fb9d526e61cee7c2a445b3
Charan Karthik <secharank@gmail.com>
2023-01-05 15:11:40 -0800

    iterative solution posted

---
+++ Week_8_Day_2.js
@@ -0,0 +1,51 @@
+class BST {
+    constructor() {
+        this.root = null;
+    }
+
+    isEmpty() {
+        return this.root === null;
+    };
+
+    insert(node, current=this.root) {
+        if(current === null){
+            this.root = node;
+            return;
+        }
+
+        if(node.value > current.value){
+            if(current.right === null){
+                current.right = node;
+                return;
+            } else {
+                this.insert(node, current.right);
+            }
+        }
+
+        if(node.value < current.value){
+            if(current.left === null){
+                current.left = node;
+                return;
+            } else {
+                this.insert(node, current.left);
+            }
+        }
+    };
+
+    getSmallestIterative(){
+        let current = this.root;
+        if (current === null){
+            return;
+        }
+
+        while (current.left != null){
+            current = current.left;
+        }
+
+        return current.value;
+    };
+
+    getSmallestRecursive(current = this.root){
+        return;
+    };
+};
