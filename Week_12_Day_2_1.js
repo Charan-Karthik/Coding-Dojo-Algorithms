93f24dfe7d0027c880b857fa0b95ae874a95d538
Charan Karthik <secharank@gmail.com>
2023-01-09 14:00:16 -0800

    mergeDedupe complete

--- Week_12_Day_2_1.js
+++ Week_12_Day_2_1.js
@@ -11,12 +11,50 @@ var arrA = [1, 3, 4, 5];
 var arrB = [1, 3, 3, 5, 8, 10,];
 
 function mergeDedupe(arr1, arr2) {
+    let res = new Set();
+    let i = 0;
+    let j = 0;
+
+    while (arr1[i] !== undefined && arr2[j] !== undefined) {
+        if (arr1[i] < arr2[j]) {
+            if (!res.has(arr1[i])) {
+                res.add(arr1[i]);
+            }
+            i += 1;
+        } else if (arr1[i] > arr2[j]) {
+            if (!res.has(arr2[j])) {
+                res.add(arr2[j]);
+            }
+            j += 1;
+        } else { // meaning arr1[i] === arr2[j]
+            if (!res.has(arr1[i])) {
+                res.add(arr1[i]);
+            }
+            i += 1;
+            j += 1;
+        }
+    }
+
+    while (i < arr1.length) {
+        if (!res.has(arr1[i])) {
+            res.add(arr1[i]);
+        }
+        i += 1;
+    }
+    while (j < arr2.length) {
+        if (!res.has(arr2[j])) {
+            res.add(arr2[j]);
+        }
+        j += 1;
+    }
+
+    return Array.from(res);
 
 }
 
 // try out some other tests
-mergeDedupe([1, 3, 3, 5, 8, 10], [1, 3, 4, 5]) // [ 1, 3, 4, 5, 8, 10 ]
-mergeDedupe([2, 3, 3, 5, 8, 10, 12], [1, 3, 4, 6]) // [1, 2, 3, 4, 5, 6, 8, 10, 12]
+console.log(mergeDedupe([1, 3, 3, 5, 8, 10], [1, 3, 4, 5])) // [ 1, 3, 4, 5, 8, 10 ]
+console.log(mergeDedupe([2, 3, 3, 5, 8, 10, 12], [1, 3, 4, 6])) // [1, 2, 3, 4, 5, 6, 8, 10, 12]
 
 //   a -> 
 //     [1, 3, 3, 5, 8, 10]
