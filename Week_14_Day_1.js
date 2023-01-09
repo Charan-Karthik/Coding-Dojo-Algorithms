20a07ec47b1df4fc60959eb0d9419f76769b7d22
Charan Karthik <secharank@gmail.com>
2023-01-09 13:14:26 -0800

    new algo initial commit

---
+++ Week_14_Day_1.js
@@ -0,0 +1,5 @@
+// Write a function that takes in two inputs. The first is an array, and the second is a callback function.
+// This function should remove every element in the array (starting with index 0) until the callback function returns true.
+// The callback function takes in the iterated element of the array.
+// Return an empty array if the callback function never returns true.
+
